fc3a33514094765325d1498c312de53d7f247db3
Alejandro Macedo <alex.macedopereira@gmail.com>
2018-03-18 15:40:47 -0500

    Added more functionality to dotfiles

--- Scripts/SyncMusicCel.sh
+++ Scripts/SyncMusicCel.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rsync -auvhnie "ssh -p 2222 -T -o Compression=no -x" usuario@$1:/storage/88B0-1EE8/Android/data/com.netease.cloudmusic/files/Documents/Music/ ~/Music
+rsync -auvhnie "ssh -p 2222 -T -o Compression=no -x" usuario@$1:/data/data/com.arachnoid.sshelper/home/SDCard/netease/cloudmusic/Music/ ~/Music
 
 echo "Do you wanna sync music? (y/n)"
 read ANS
 
 if [[ $ANS = 'y' ]]
 then
-   rsync -auvhie "ssh -p 2222 -T -o Compression=no -x" usuario@$1:/storage/88B0-1EE8/Android/data/com.netease.cloudmusic/files/Documents/Music/ ~/Music
+   rsync -auvhie "ssh -p 2222 -T -o Compression=no -x" usuario@$1:/data/data/com.arachnoid.sshelper/home/SDCard/netease/cloudmusic/Music/ ~/Music
 else
    echo "Not syncing music"
 fi
