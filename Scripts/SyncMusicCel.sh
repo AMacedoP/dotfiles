ee7ea1e73d9c9ec52a91fd3df86b501c73077621
Alejandro Macedo <alex.macedopereira@gmail.com>
2018-05-23 01:05:31 -0500

    Changed scripts

--- Scripts/SyncMusicCel.sh
+++ Scripts/SyncMusicCel.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rsync -auvhnie "ssh -p 2222 -T -o Compression=no -x" usuario@$1:/data/data/com.arachnoid.sshelper/home/SDCard/netease/cloudmusic/Music/ ~/Music
+rsync -auvhnie "ssh -p 2222 -T -o Compression=no -x" usuario@$1:/sdcard/netease/cloudmusic/Music/ ~/Music
 
 echo "Do you wanna sync music? (y/n)"
 read ANS
 
 if [[ $ANS = 'y' ]]
 then
-   rsync -auvhie "ssh -p 2222 -T -o Compression=no -x" usuario@$1:/data/data/com.arachnoid.sshelper/home/SDCard/netease/cloudmusic/Music/ ~/Music
+   rsync -auvhiPe "ssh -p 2222 -T -o Compression=no -x" usuario@$1:/sdcard/netease/cloudmusic/Music/ ~/Music
 else
    echo "Not syncing music"
 fi
