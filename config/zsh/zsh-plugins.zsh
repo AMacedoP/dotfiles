07086fce9a53fa5f178b692b4efeeb912367c57f
Alejandro Macedo <alex.macedopereira@gmail.com>
2023-12-24 16:53:04 -0500

    moving away from ohmyzsh
    Signed-off-by: Alejandro Macedo <alex.macedopereira@gmail.com>

---
+++ config/zsh/zsh-plugins.zsh
@@ -0,0 +1,2 @@
+fpath+=( /home/alex/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-trystan2k-SLASH-zsh-tab-title )
+source /home/alex/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-trystan2k-SLASH-zsh-tab-title/zsh-tab-title.plugin.zsh
